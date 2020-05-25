e53ccac41a13e4aff8d271f25cfce82b0d05c4bd
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-25 03:26:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,7 +46,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by_id
+  Movie.find
 end
 
 def can_find_by_multiple_attributes
@@ -54,7 +54,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  movie.find_by(params[:id])
+  Movie.find
 end
 
 def can_find_using_where_clause_and_be_sorted
