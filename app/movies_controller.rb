e21fa1e6663d6a57d26c9e836230357fd41f41d8
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-25 04:25:16 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -66,8 +66,7 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  binding.pry
-  Movie.title = "Even Awesome Flick"
+  var = Movie.find(1)
   Movie.save
   Movie.title
 end
